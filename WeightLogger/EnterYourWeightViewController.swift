d8f79df11f7fc84f738541d585559c0fd29e5a45
Tony Abboud <tony.abboud54@gmail.com>
2014-10-02 21:18:33 -0400

    Updated for Xcode 6.0.1 GM

--- WeightLogger/EnterYourWeightViewController.swift
+++ WeightLogger/EnterYourWeightViewController.swift
@@ -11,8 +11,8 @@ import CoreData
 
 
 class EnterYourWeightViewController: UIViewController {
-
-
+    
+    
     @IBOutlet var txtWeight : UITextField!
     @IBOutlet var units : UISwitch!
     
@@ -22,9 +22,10 @@ class EnterYourWeightViewController: UIViewController {
         if(!txtWeight.text.isEmpty){
             //save data
             var appDel: AppDelegate = (UIApplication.sharedApplication().delegate as AppDelegate)
-            var context: NSManagedObjectContext = appDel.managedObjectContext
+            var context: NSManagedObjectContext = appDel.managedObjectContext!
             
-            let ent = NSEntityDescription.entityForName("UserWeights", inManagedObjectContext: context)
+            
+            let ent = NSEntityDescription.entityForName("UserWeights", inManagedObjectContext: context)!
             
             //Instance of our custom class, reference to entity
             var newWeight = UserWeights(entity: ent, insertIntoManagedObjectContext: context)
@@ -40,56 +41,37 @@ class EnterYourWeightViewController: UIViewController {
             
             let dateFormatter = NSDateFormatter()
             var curLocale: NSLocale = NSLocale.currentLocale()
-            var formatString: NSString = NSDateFormatter.dateFormatFromTemplate("EdMMM h:mm a", options: 0, locale: curLocale)
+            var formatString: NSString = NSDateFormatter.dateFormatFromTemplate("EdMMM h:mm a", options: 0, locale: curLocale)!
             dateFormatter.dateFormat = formatString
             newWeight.date = dateFormatter.stringFromDate(NSDate())
             
             context.save(nil)
-            
-            println(newWeight)
-            println(NSDate())
         }else{
             //User carelessly pressed save button without entering weight.
             var alert:UIAlertController = UIAlertController(title: "No Weight Entered", message: "Enter your weight before pressing save.", preferredStyle: .Alert)
             alert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: {(result)in
-                    alert.dismissViewControllerAnimated(true, completion: nil)
-                }))
+                alert.dismissViewControllerAnimated(true, completion: nil)
+            }))
             self.presentViewController(alert, animated: true, completion: nil)
         }
         
     }
     
-    override func shouldPerformSegueWithIdentifier(identifier: String!, sender: AnyObject!) -> Bool {
-        println("val: \(txtWeight.text.isEmpty) and \(identifier)")
-        var val = (identifier != nil && txtWeight.text.isEmpty ? false:true)
-        txtWeight.text = ""
-        return val
-    }
     
-//Hide the keyboard
-//    func textFieldShouldReturn(textField: UITextField!) ->Bool{
-//        txtWeight.resignFirstResponder()
-//        return true
-//    }
-//    
-    
-    override func touchesBegan(touches: NSSet!, withEvent event: UIEvent!){
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         self.view.endEditing(true)
     }
     
-    
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-
+    override func viewWillAppear(animated: Bool) {
+        txtWeight.text = ""
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
--- WeightLogger/WeightLogTableViewController.swift
+++ WeightLogger/WeightLogTableViewController.swift
@@ -14,14 +14,14 @@ class WeightLogTableViewController: UITableViewController, UITableViewDelegate, 
     var totalEntries: Int = 0
     
     @IBOutlet var tblLog : UITableView?
-
+    
     @IBAction func btnClearLog(sender : AnyObject) {
         var appDel = (UIApplication.sharedApplication().delegate as AppDelegate)
-        var context = appDel.managedObjectContext
+        var context = appDel.managedObjectContext!
         
         var request = NSFetchRequest(entityName: "UserWeights")
         request.returnsObjectsAsFaults = false
-        var results: NSArray = context.executeFetchRequest(request, error: nil)
+        var results: NSArray = context.executeFetchRequest(request, error: nil)!
         
         for weightEntry: AnyObject in results{
             context.deleteObject(weightEntry as NSManagedObject)
@@ -30,7 +30,7 @@ class WeightLogTableViewController: UITableViewController, UITableViewDelegate, 
         totalEntries = 0
         tblLog?.reloadData()
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         var appDel = (UIApplication.sharedApplication().delegate as AppDelegate)
@@ -39,51 +39,46 @@ class WeightLogTableViewController: UITableViewController, UITableViewDelegate, 
         var request = NSFetchRequest(entityName: "UserWeights")
         request.returnsObjectsAsFaults = false
         
-        totalEntries = context.countForFetchRequest(request, error: nil)
-        //println(totalEntries)
+        totalEntries = context?.countForFetchRequest(request, error: nil) as Int!
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-    // #pragma mark - Table view data source
-
+    
+// #pragma mark - UITableViewDataSource Methods
     override func numberOfSectionsInTableView(tableView: UITableView?) -> Int {
-        // #warning Potentially incomplete method implementation.
-        // Return the number of sections.
         return 1
     }
-
-    override func tableView(tableView: UITableView?, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete method implementation.
-        // Return the number of rows in the section.
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return totalEntries
     }
-
-
-    override func tableView(tableView: UITableView?, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell? {
+    
+    
+    
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
         let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "Default")
         var appDel = (UIApplication.sharedApplication().delegate as AppDelegate)
         var context = appDel.managedObjectContext
         var request = NSFetchRequest(entityName: "UserWeights")
         request.returnsObjectsAsFaults = false
         
-        var results: NSArray = context.executeFetchRequest(request, error: nil)
+        var results: NSArray = context?.executeFetchRequest(request, error: nil) as NSArray!
         
         //get contents and put into cell
         var thisWeight: UserWeights = results[indexPath.row] as UserWeights
-        cell.textLabel.text = thisWeight.weight + " " + thisWeight.units
-        cell.detailTextLabel.text = thisWeight.date
+        cell.textLabel?.text = thisWeight.weight + " " + thisWeight.units
+        cell.detailTextLabel?.text = thisWeight.date
         return cell
     }
-
-    override func tableView(tableView: UITableView!, canEditRowAtIndexPath indexPath: NSIndexPath!) -> Bool{
-     return true
+    
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool{
+        return true
     }
     
-   override func tableView(tableView: UITableView!, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath!){
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath){
         //delete object from entity, remove from list
         let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "Default")
         var appDel = (UIApplication.sharedApplication().delegate as AppDelegate)
@@ -91,14 +86,18 @@ class WeightLogTableViewController: UITableViewController, UITableViewDelegate, 
         var request = NSFetchRequest(entityName: "UserWeights")
         request.returnsObjectsAsFaults = false
         
-        var results: NSArray = context.executeFetchRequest(request, error: nil)
+        let results: NSArray = context?.executeFetchRequest(request, error: nil) as NSArray!
         
-        println("Object being deleted: \(results[indexPath.row])")
-        context.deleteObject(results[indexPath.row] as NSManagedObject)
-        context.save(nil)
+        //Get value that is being deeleted
+        let tmpObject: NSManagedObject = results[indexPath.row] as NSManagedObject
+        let delWeight = tmpObject.valueForKey("weight") as String
+        println("Deleted Weight: \(delWeight)")
+        
+        context?.deleteObject(results[indexPath.row] as NSManagedObject)
+        context?.save(nil)
         totalEntries = totalEntries - 1
-        tblLog?.reloadData()
+        tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
         println("Done")
     }
-
+    
 }
